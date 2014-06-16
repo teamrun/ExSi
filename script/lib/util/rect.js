618188952ca9de2d766212ba88d23bad4bc095fe
teamrun <chenllos@163.com>
2014-06-16 17:00:57 +0800

    换用相对于document左上角的绝对坐标来实现功能

--- script/lib/util/rect.js
+++ script/lib/util/rect.js
@@ -2,6 +2,7 @@
 
     var style = document.createElement('style');
     style.innerHTML = '#m-rect-dom{border-radius: 4px;width: 0px;height:0px;background: rgba(50,50,50,0.25);}';
+    style.innerHTML += '.rect-ctn{position:relative; user-select:none;}';
     document.body.appendChild(style)
 
     if( !Function.bind ){
@@ -16,9 +17,11 @@
         return Number(valueStr.substr(0, valueStr.length-2))
     }
 
-    var RECT_ID = 'm-rect-dom'
+    var RECT_ID = 'm-rect-dom';
+    var RECT_CTN_CLASS = 'rect-ctn';
     window.MouseRect = function( opt ){
         this.ctn = $(opt.ctn);
+        this.ctn.addClass(RECT_CTN_CLASS);
 
         var rectEle = $(document.createElement('div'));
         rectEle.attr('id', RECT_ID);
@@ -28,11 +31,17 @@
 
         this.startX = 0;
         this.startY = 0;
+
+        this.updateCtnPos();
         this.monitFlag = false;
 
         this.bind();
 
         return this;
+    };
+
+    MouseRect.prototype.updateCtnPos = function() {
+        this.ctnPos = this.ctn.offset();
     }
 
     MouseRect.prototype.bind = function() {
@@ -54,13 +63,17 @@
         });
     }
 
+    function getOffset(el){
+
+    }
     function startRect(e){
         this.monitFlag = true;
         // console.log(e) 
         // offsetX/Y 基于父级中"定位"(pos值为relative or abs)了的元素的位置
-        // console.log('set rect start: ', e.offsetX, e.offsetY )
-        this.startX = e.offsetX
-        this.startY = e.offsetY
+        // 当ctn中有其他dom时会出现offset基准变更的问题, 所以干脆用pageX
+        console.log('set rect start: ', e.pageX, e.pageY )
+        this.startX = e.pageX - this.ctnPos.left;
+        this.startY = e.pageY - this.ctnPos.top;
         this.rectEle.css({
             top:  this.startY,
             left: this.startX
@@ -70,58 +83,35 @@
         var styleObj = {}
         // 鼠标滑动速度过快或者发生急速的变相时,指针会到rect上去
         // 此时的offset的基准会发生变化...
-        if( $(e.target).attr('id') == RECT_ID ){
-            // console.log('鼠标滑到了rect上...');
-            styleObj = {
-                width: e.offsetX,
-                height: e.offsetY,
-            };
-            // 右下向左上划时...
-            var cur = {
-                top: getStyleValue(this.rectEle.css('top')),
-                left: getStyleValue(this.rectEle.css('left')),
-                width: getStyleValue(this.rectEle.css('width')),
-                height: getStyleValue(this.rectEle.css('height'))
-            };
-            if(cur.top != this.startY){
-                // console.log('up');
-                // console.log( cur.top + e.offsetY )
-                // console.log( cur.height - e.offsetY )
-                styleObj.top = cur.top + e.offsetY
-                styleObj.height = cur.height - e.offsetY;
-            }
-            if(cur.left != this.startX){
-                // console.log('left');
-                styleObj.left = cur.left + e.offsetX;
-                styleObj.width = cur.width - e.offsetX;
-            }
+        // 干脆换用pageX
+        // mousePos relative to this.ctn
+        var mousePosX = e.pageX - this.ctnPos.left;
+        var mousePosY = e.pageY - this.ctnPos.top;
+        var deltaX = mousePosX - this.startX;
+        var deltaY = mousePosY - this.startY;
+        // console.log('rect W: %d ,H: %d', deltaX, deltaY);
+        styleObj = {
+            width: Math.abs(deltaX),
+            height: Math.abs(deltaY),
+        };
+        // 右下向左上划时...
+        if(deltaX < 0){
+            styleObj.left = mousePosX
         }
         else{
-            var deltaX = e.offsetX - this.startX;
-            var deltaY = e.offsetY - this.startY;
-            // console.log('rect W: %d ,H: %d', deltaX, deltaY);
-            styleObj = {
-                width: Math.abs(deltaX),
-                height: Math.abs(deltaY),
-            };
-            // 右下向左上划时...
-            if(deltaX < 0){
-                styleObj.left = e.offsetX
-            }
-            else{
-                styleObj.left = this.startX
-            }
-            if(deltaY < 0){
-                styleObj.top = e.offsetY
-            }
-            else{
-                styleObj.top = this.startY
-            }
+            styleObj.left = this.startX
+        }
+        if(deltaY < 0){
+            styleObj.top = mousePosY
+        }
+        else{
+            styleObj.top = this.startY
         }
         // 直接赋值css会有bug jquery的bug
-        for(var i in styleObj){
-            this.rectEle.css(i, styleObj[i]);
-        }
+        // for(var i in styleObj){
+        //     this.rectEle.css(i, styleObj[i]);
+        // }
+        this.rectEle.css( styleObj );
         if(callback instanceof Function){
             callback(styleObj);
         }
